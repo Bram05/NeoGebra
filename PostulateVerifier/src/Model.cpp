3c077503bcebe9c6d652c0f47aa5425433d10597
JoenPoenMan <jeroenkrijgveel@gmail.com>
2022-11-29 21:20:34 +0100

    FloatingPoint weer weggehaald [was te sloom]
    This reverts commit e2e724db349b31420c7775e8744d311cef34dae0.

--- PostulateVerifier/src/Model.cpp
+++ PostulateVerifier/src/Model.cpp
@@ -28,7 +28,7 @@ point Model::newPoint(const std::vector<float>& identifiers) {
 		throw std::invalid_argument("Invalid point");
 	}
 
-	if (!Z3Tools::isSolvable(m_PointDef, { identifiers }).sat) {
+	if (!Z3Tools::isSolvable(m_PointDef, { identifiers })) {
 		throw std::invalid_argument("Invalid point");
 	}
 
@@ -53,7 +53,7 @@ line Model::newLine(const std::vector<float>& identifiers) {
 		throw std::invalid_argument("Invalid line");
 	}
 
-	if (!Z3Tools::isSolvable(m_LineDef, { identifiers }).sat) {
+	if (!Z3Tools::isSolvable(m_LineDef, { identifiers })) {
 		throw std::invalid_argument("Invalid line");
 	}
 
@@ -69,22 +69,9 @@ line Model::newLine(const std::vector<float>& identifiers) {
 	return l;
 }
 
-line Model::newLine(point p1, point p2) {
-	equation halfEq = m_IncidenceConstr;
-	halfEq.vars.erase(std::next(halfEq.vars.begin()));
-	equation eq = halfEq + halfEq;
-	equationResult res = Z3Tools::isSolvable(eq, { p1.identifiers, p2.identifiers });
-	if (!res.sat) {
-		throw std::invalid_argument("I-1 failed");
-	}
-	for (int i = 0; i < (*res.m).num_consts(); ++i) {
-		z3::func_decl f = (*res.m).get_const_decl(i);
-		std::cout << f.name() << std::endl;
-		
-	}
-
-	return newLine({ 1.25, 0 });
-}
+//line newLine(point p1, point p2) {
+//
+//}
 
 bool operator==(const point lhs, const point rhs) {
 	if (lhs.m != rhs.m) {
@@ -94,8 +81,8 @@ bool operator==(const point lhs, const point rhs) {
 
 	equation constr1 = (*lhs.m).m_PointDef + !(*lhs.m).m_PointDef;
 	equation constr2 = !(*lhs.m).m_PointDef + (*lhs.m).m_PointDef;
-	if (Z3Tools::isSolvable(constr1, { lhs.identifiers, rhs.identifiers }).sat or 
-		Z3Tools::isSolvable(constr2, { lhs.identifiers, rhs.identifiers }).sat) {
+	if (Z3Tools::isSolvable(constr1, { lhs.identifiers, rhs.identifiers }) or 
+		Z3Tools::isSolvable(constr2, { lhs.identifiers, rhs.identifiers })) {
 		return false;
 	}
 	else {
@@ -111,8 +98,8 @@ bool operator==(const line lhs, const line rhs) {
 
 	equation constr1 = (*lhs.m).m_LineDef + !(*lhs.m).m_LineDef;
 	equation constr2 = !(*lhs.m).m_LineDef + (*lhs.m).m_LineDef;
-	if (Z3Tools::isSolvable(constr1, { lhs.identifiers, rhs.identifiers }).sat or
-		Z3Tools::isSolvable(constr2, { lhs.identifiers, rhs.identifiers }).sat) {
+	if (Z3Tools::isSolvable(constr1, { lhs.identifiers, rhs.identifiers }) or
+		Z3Tools::isSolvable(constr2, { lhs.identifiers, rhs.identifiers })) {
 		return false;
 	}
 	else {
@@ -141,21 +128,20 @@ bool isBetween(const point p1, const point p2, const point p3) {
 	}
 
 	//Check if the 3 points lie on the same line
-	//std::string lineConstraints[3] = { (*p1.m).m_LineDef.eq, (*p1.m).m_LineDef.eq, (*p1.m).m_LineDef.eq };
-	//equation pointEquations[3] = { (*p1.m).m_PointDef, (*p1.m).m_PointDef, (*p1.m).m_PointDef };
-	//for (int i = 0; i < 3; ++i) {
-	//	Z3Tools::replaceVar(lineConstraints[i], "x", std::string{ "x" } + (char)('a' + i));
-	//	Z3Tools::replaceVar(lineConstraints[i], "y", std::string{ "y" } + (char)('a' + i));
-	//	Z3Tools::replaceVar(pointEquations[i].eq, "x", std::string{ "x" } + (char)('a' + i));
-	//	Z3Tools::replaceVar(pointEquations[i].eq, "y", std::string{ "y" } + (char)('a' + i));
-	//}
-	//
-	//equation lineEq{ {}, lineConstraints[0] + '&' + lineConstraints[1] + '&' + lineConstraints[2] };
-	//equation totalEq = pointEquations[0] + pointEquations[1] + pointEquations[2] + lineEq;
-	//if (!Z3Tools::isSolvable(totalEq, { p1.identifiers, p2.identifiers, p3.identifiers }).sat) {
-	//	return false;
-	//}
-	line l = (*p1.m).newLine(p1, p2);
+	std::string lineConstraints[3] = { (*p1.m).m_LineDef.eq, (*p1.m).m_LineDef.eq, (*p1.m).m_LineDef.eq };
+	equation pointEquations[3] = { (*p1.m).m_PointDef, (*p1.m).m_PointDef, (*p1.m).m_PointDef };
+	for (int i = 0; i < 3; ++i) {
+		Z3Tools::replaceVar(lineConstraints[i], "x", std::string{ "x" } + (char)('a' + i));
+		Z3Tools::replaceVar(lineConstraints[i], "y", std::string{ "y" } + (char)('a' + i));
+		Z3Tools::replaceVar(pointEquations[i].eq, "x", std::string{ "x" } + (char)('a' + i));
+		Z3Tools::replaceVar(pointEquations[i].eq, "y", std::string{ "y" } + (char)('a' + i));
+	}
+
+	equation lineEq{ {}, lineConstraints[0] + '&' + lineConstraints[1] + '&' + lineConstraints[2] };
+	equation totalEq = pointEquations[0] + pointEquations[1] + pointEquations[2] + lineEq;
+	if (!Z3Tools::isSolvable(totalEq, { p1.identifiers, p2.identifiers, p3.identifiers })) {
+		return false;
+	}
 
 	//Custom condition
 	equation eq = (*p1.m).m_BetweennessConstr;
