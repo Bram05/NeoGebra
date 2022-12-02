d6b272b20bfa535fb2581b36f3a028c1c45a4f13
Riad Zaid <62952229+RiadZX@users.noreply.github.com>
2022-12-02 20:14:06 +0100

    create files

---
+++ NietEuclidischeGeogebra/src/TextRenderer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <vector>
+#include <iostream>
+#include <fstream> 
+
+#include "shader_configure.h"
+#include "text_fonts_glyphs.h"
+
